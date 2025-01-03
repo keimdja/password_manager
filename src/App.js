fe2bb6a7d9ab17a0cd1aff567f1282f6fcc9ca7c
keimdja <keirelysmdja8@gmail.com>
2025-01-03 11:02:06 -0400

    First setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Password Manager</h1>
+      <p>Welcome to your Password Manager App!</p>
+    </div>
+  );
+}
+
+export default App;
